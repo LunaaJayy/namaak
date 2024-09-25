56617cf4118dd8fbaef824050f62c37167f74b77
LunaaJayy <152217714+LunaaJayy@users.noreply.github.com>
2024-09-25 12:01:06 +0200

    hamburgermenu af

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
 console.log("hi");
+
+let openButton = document.querySelector(".h-onderste button");
+let sluitButton = document.querySelector("nav button")
+let deNav = document.querySelector("nav")
+
+openButton.addEventListener("click", openMenu)
+sluitButton.addEventListener("click", sluitMenu)
+
+function openMenu() {
+    deNav.classList.add("toonMenu");
+}
+
+function sluitMenu() {
+    deNav.classList.remove("toonMenu");
+}
