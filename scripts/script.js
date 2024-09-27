4e5adc360cde872ee7deacbc42b82d6e2809b819
LunaaJayy <152217714+LunaaJayy@users.noreply.github.com>
2024-09-27 11:40:51 +0200

    javascript basis af (nu nog toevoegingen)

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,17 @@ let openButton = document.querySelector(".h-onderste button");
 let sluitButton = document.querySelector("nav button")
 let deNav = document.querySelector("nav")
 
+const prijsSlider = document.querySelector(".prijsslider")
+const procentSlider = document.querySelector(".procentslider")
+let verkoopVerander = document.querySelector(".verkoop")
+let vergelijkingVerander = document.querySelector(".vergelijking")
+let prijsVerandering = document.querySelector(".deprijs")
+
+let valuePrijs = prijsSlider.value
+let valueProcent = procentSlider.value
+
+berekenBesparing()
+
 openButton.addEventListener("click", openMenu)
 sluitButton.addEventListener("click", sluitMenu)
 
@@ -15,3 +26,29 @@ function openMenu() {
 function sluitMenu() {
     deNav.classList.remove("toonMenu");
 }
+
+verkoopVerander.textContent = '€ ' + valuePrijs;
+vergelijkingVerander.textContent = valueProcent + '%';
+
+prijsSlider.addEventListener('input', updatePrijs)
+procentSlider.addEventListener('input', updateProcent)
+
+function updatePrijs() {
+    verkoopVerander.textContent = '€ ' + this.value 
+    valuePrijs = this.value
+    berekenBesparing()
+}
+
+function updateProcent() {
+    vergelijkingVerander.textContent = this.value + '%'
+    valueProcent = this.value
+    berekenBesparing()
+}
+
+
+function berekenBesparing() {
+    let berekeningBesp = valuePrijs * valueProcent / 100 - 1525;
+    console.log(berekeningBesp)
+    prijsVerandering.textContent = berekeningBesp
+}
+
